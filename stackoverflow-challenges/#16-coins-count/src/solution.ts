492a11aa544b33f854008cc4b9c26e5f55854ba9
vickbk <vickbake8@gmail.com>
2026-03-03 23:50:24 +0200

    improving code readability

--- stackoverflow-challenges/#16-coins-count/src/solution.ts
+++ stackoverflow-challenges/#16-coins-count/src/solution.ts
@@ -1,37 +1,38 @@
-import { default as input } from "../assets/inputs.json" with { type: "json" };
+import { default as inputs } from "../assets/inputs.json" with { type: "json" };
 
 type Coins = [number[], number[], number];
 
 function countCoins(
-  [coins, count, target]: Coins,
+  [coins, counts, target]: Coins,
   memo: Map<string, number> = new Map(),
 ): number {
-  const key = `${coins.join(",")}-${count.join(",")}-${target}`;
+  const key = `${coins.join(",")}-${counts.join(",")}-${target}`;
   if (memo.has(key)) return memo.get(key)!;
 
   if (target === 0) {
     memo.set(key, 0);
     return 0;
   }
-  if (target < 0 || count.every((c) => c === 0)) {
+  if (target < 0 || counts.every((count) => count === 0)) {
     memo.set(key, -1);
     return -1;
   }
 
   const allCounts = coins
     .map((coin, i) => {
-      if (count[i] === 0) return -1;
-      const newCount = count.map((c, j) => (j === i ? c - 1 : c));
+      if (counts[i] === 0) return -1;
+      const newCount = counts.map((count, j) => (j === i ? count - 1 : count));
       const result = countCoins([coins, newCount, target - coin], memo);
       return result === -1 ? -1 : result + 1;
     })
-    .filter((c) => c !== -1);
+    .filter((c) => c !== -1)
+    .sort((a, b) => a - b);
 
-  const minCount = allCounts.length > 0 ? Math.min(...allCounts) : -1;
+  const [minCount = -1] = allCounts;
   memo.set(key, minCount);
   return minCount;
 }
 
-(input as Coins[]).forEach((input) => {
+(inputs as Coins[]).forEach((input) => {
   console.log({ input, result: countCoins(input) });
 });
